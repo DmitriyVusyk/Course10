a4fd9522a654ec52e52944afd95dd193370926fd
DmitriyVusyk <kotmurkot4502@gmail.com>
2017-11-28 11:26:33 +0100

    module1

---
+++ src/module1/Main.java
@@ -0,0 +1,11 @@
+package module1;
+
+/**
+ * Created by dmytrovusyk
+ */
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Я провел лето хреново ;)");
+    }
+}
